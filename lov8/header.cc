78d8823dbe8179cccf3277c5e18d4a109467e82a
m4rvin2005 <m4rvin2005@8b5f54a0-8722-11de-9e21-49812d2d8162>
2009-12-16 15:57:43 +0000

    added demos

--- lov8/header.cc
+++
@@ -1,8 +0,0 @@
-<?php foreach($this->objects as $object) : ?>
-#include "<?php echo strtolower($object['name']);?>.h";
-<?php endforeach; ?>
-
-<?php foreach($this->objects as $object) : ?>
-void JS<?php echo $object['name'];?>SetupClass(v8::Handle<v8::ObjectTemplate> js_obj);
-v8::Handle<v8::FunctionTemplate> JS<?php echo $object['name'];?>CreateClass();
-<?php endforeach; ?>
